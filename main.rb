9abcddffabc41a9ded58c0cf1a4a41cba1382998
Tomo Hung <tomohung@gmail.com>
2015-01-10 17:03:04 +0800

    add  login, bets, game

--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
 require 'sinatra'
 require 'pry'
 
+set :sessions, true
+
 get '/' do
-  redirect '/login' if session[:username].nil?
-  redirect '/bet'
+  redirect '/login'
 end
 
 get '/login' do
@@ -17,5 +18,17 @@ end
 post '/set_name' do
   redirect '/login' if params[:username].empty?
   session[:username] = params[:username]
+  session[:capital] = 500
   redirect '/bet'
+end
+
+post '/set_bet' do  
+  redirect '/bet' if params[:bet].to_i < 1
+  session[:bet] = params[:bet].to_i
+  session[:capital] -= params[:bet].to_i
+  redirect '/game'
+end
+
+get '/game' do
+  erb :game
 end
